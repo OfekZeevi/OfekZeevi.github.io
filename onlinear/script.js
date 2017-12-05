2bc0ba6c83aec806aa2de830c0853ba3f5a263a8
Ofek <zeevi.ofek@gmail.com>
2017-12-05 16:19:30 +0200

    Tidying Up The Code...

--- onlinear/script.js
+++ onlinear/script.js
@@ -1,18 +1,10 @@
 window.onload = function () {
 
-  /*// Normalize the various vendor prefixed versions of getUserMedia.
-  navigator.getUserMedia = (navigator.getUserMedia ||
-                            navigator.webkitGetUserMedia ||
-                            navigator.mozGetUserMedia || 
-                            navigator.msGetUserMedia);*/
-
-  var constraints = { video: { 
-                   facingMode: "environment"  
-                   /*mandatory: {
-                                maxWidth: _this.parameters.sourceWidth,
-		                       maxHeight: _this.parameters.sourceHeight
-		    		}*/
-}};
+    var constraints = { 
+        video: { 
+            facingMode: "environment"
+        }
+    };
   
     function handleSuccess(stream)
     {
@@ -20,79 +12,21 @@ window.onload = function () {
         var vid = document.getElementById('camera-stream');
         
         var videoTracks = stream.getVideoTracks();
-          console.log('Got stream with constraints:', constraints);
-          console.log('Using video device: ' + videoTracks[0].label);
-          stream.oninactive = function() {
+        console.log('Got stream with constraints:', constraints);
+        console.log('Using video device: ' + videoTracks[0].label);
+        stream.oninactive = function() {
             console.log('Stream inactive');
-          };
-          window.stream = stream; // make variable available to browser console
-          vid.srcObject = stream;
-        
-        /*
-
-        // Create an object URL for the video stream and use this 
-        // to set the video source.
-        var binaryData = [];
-        binaryData.push(localMediaStream);
-        var blob = new Blob(binaryData, {type: "application/zip"});
-        vid.src = window.URL.createObjectURL(blob);//localMediaStream);*/
+        };
+        window.stream = stream; // make variable available to browser console
+        vid.srcObject = stream;
     }
     
     function handleError(err)
     {
-      // Log the error to the console.
-    console.log('The following error occurred when trying to use getUserMedia: ' + err);
+        // Log the error to the console.
+        console.log('The following error occurred when trying to use getUserMedia: ' + err);
     }
    
-    
-    navigator.mediaDevices.getUserMedia(constraints).
-    then(handleSuccess).catch(handleError);
-
-    
-    /*
-// Check that the browser supports getUserMedia.
-// If it doesn't show an alert, otherwise continue.
-if (navigator.getUserMedia) {
-  // Request the camera.
-  navigator.getUserMedia(
-    // Constraints
-    {
-      video: true
-    },
-
-    // Success Callback
-    function(localMediaStream) {
-        // Get a reference to the video element on the page.
-        var vid = document.getElementById('camera-stream');
-        
-        var videoTracks = localMediaStream.getVideoTracks();
-          //console.log('Got stream with constraints:', constraints);
-          //console.log('Using video device: ' + videoTracks[0].label);
-          localMediaStream.oninactive = function() {
-            console.log('Stream inactive');
-          };
-          window.stream = localMediaStream; // make variable available to browser console
-          vid.srcObject = localMediaStream;
-        
-        /*
-
-        // Create an object URL for the video stream and use this 
-        // to set the video source.
-        var binaryData = [];
-        binaryData.push(localMediaStream);
-        var blob = new Blob(binaryData, {type: "application/zip"});
-        vid.src = window.URL.createObjectURL(blob);//localMediaStream);*
-    },
-
-    // Error Callback
-    function(err) {
-      // Log the error to the console.
-      console.log('The following error occurred when trying to use getUserMedia: ' + err);
-    }
-  );
-
-} else {
-  alert('Sorry, your browser does not support getUserMedia');
-}*/
+    navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
     
 }
