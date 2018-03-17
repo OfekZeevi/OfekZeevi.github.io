f9d1d1ab0014c66bc86b67baf7943f7d6e4149bf
Ofek <zeevi.ofek@gmail.com>
2018-03-17 19:12:27 +0200

    Added more "skeleton" related functions

--- Hantar/code.js
+++ Hantar/code.js
@@ -2,6 +2,8 @@ var LS_PREFIX = "hantar_";
 var LS_KEY_SESSION_ID = LS_PREFIX + "session_id";
 var LS_KEY_LIST_SIGNATURE = LS_PREFIX + "list_signature";
 
+var LIST_SIGNATURE_REGEX = /^(?:(?:^|,)(\d+(-\d+)?))+$/g
+
 function log(str) { console.log(str); }
 
 function range(start, end, step=1)
@@ -16,23 +18,67 @@ function range(start, end, step=1)
     return list;
 }
 
+function sort(arr, remove_dups=false)
+{
+    arr.sort(function(a, b) { return (a - b); });
+    
+    if (remove_dups)
+    {
+        arr = arr.filter(function(item, index) { return arr.indexOf(item) == index });
+    }
+    
+    return arr;
+}
+
 function gen_num_list(list_signature)
 {
-    var list = []
+    var list = [];
     var items = list_signature.split(",");
     var range_parts;
     for (var i = 0, len = items.length; i < len; i++)
     {
         if (items[i].indexOf("-") == -1)
         {
-            list.push(items[i] * 1)
+            list.push(items[i] * 1);
         }
         else
         {
             range_parts = items[i].split("-");
-            list = list.concat(range(range_parts[0] * 1, range_parts[1] * 1))
+            list = list.concat(range(range_parts[0] * 1, range_parts[1] * 1));
         }
     }
+    return list;
+}
+
+function gen_list_signature(num_list)
+{
+    var item = num_list[0];
+    var prev_item;
+    var list_signature = num_list[0];
+    var range_count = 1;
+    for (var i = 1, len = num_list.length; i < len; i++)
+    {
+        prev_item = item;
+        item = num_list[i];
+        if (item == prev_item + 1)
+        {
+            range_count++;
+            if (i == len - 1)
+            {
+                list_signature += "-" + item;
+            }
+        }
+        else
+        {
+            if (range_count > 1)
+            {
+                list_signature += "-" + prev_item;
+            }
+            list_signature += "," + item;
+            range_count = 1;
+        }
+    }
+    return list_signature;
 }
 
 function prev_session_exists()
